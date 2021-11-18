8cada58a821b3764d8b0b2a26fa6d8b1efbb2035
Pranali209 <69621580+Pranali209@users.noreply.github.com>
2021-11-17 20:48:58 -0800

    Add files via upload
    Bal

---
+++ PaymentBal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BalProject
+{
+  public  class PaymentBal
+    {
+      
+            public string BankName { get; set; }
+            public string IFSCCode { get; set; }
+            public string UPINo { get; set; }
+            public long CardNo { get; set; }
+            public long CVVNo { get; set; }
+        
+    }
+}
